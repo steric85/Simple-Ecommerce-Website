5546cf7e31dba3d00cb99a4d8e10a96efbbb8245
Aneree Mehta <aneemeh@gmail.com>
2018-02-26 18:21:39 +0530

    Removes async, commented out (old) code

--- js/header.js
+++ js/header.js
@@ -1,35 +1,3 @@
-// const HEADER = (function(){
-//
-//   const octopus = {
-//     init: function(){
-//       view.init();
-//     },
-//     getCartCount: function(){
-//       const cartItemList = DATABASE.getCartItemList();
-//       return cartItemList.reduce((accumulator, currentItem) => accumulator + currentItem.quantity,0);
-//     }
-//   };
-//
-//   const view = {
-//     init: function(){
-//       this.countElement =  document.getElementById('cart-items-count');
-//       this.render();
-//     },
-//     render: function(){
-//       let cartCount = octopus.getCartCount();
-//       this.countElement.textContent = cartCount;
-//     }
-//   };
-//
-//   octopus.init();
-//
-//   return {
-//     displayCartCount : ()=>view.render(),
-//   }
-// })();
-
-// const header = document.getElementsByTagName("header")[0];
-
 function Header(props){
   return (
     <header>
